8dc0c085a8c568603aeac9b9c728a63dfb43264a
Ali Zahid <ali.zahid@live.com>
2020-05-13 22:05:48 +0400

    Filters updated and tooltips added

--- pages/heroes/index.tsx
+++ pages/heroes/index.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 
 const Heroes: NextPage<Props> = ({ heroes, roles }) => {
-  const [role, setRole] = useState('all')
+  const [role, setRole] = useState('All')
 
   return (
     <>
@@ -23,14 +23,14 @@ const Heroes: NextPage<Props> = ({ heroes, roles }) => {
 
       <main>
         <h1 className="font-medium text-4xl">Heroes</h1>
-        <header className="-m-2 mt-4 flex flex-wrap items-start">
+        <header className="-m-2 mt-6 flex flex-wrap items-start">
           {['All', ...roles].map((item, index) => (
             <button
               className={`w-auto text-white m-2 p-3 font-medium leading-none ${
-                item.toLowerCase() === role ? 'bg-red-700' : 'bg-gray-800'
+                item === role ? 'bg-red-700' : 'bg-gray-800'
               }`}
               key={index}
-              onClick={() => setRole(item.toLowerCase())}>
+              onClick={() => setRole(item)}>
               {item}
             </button>
           ))}
@@ -46,21 +46,30 @@ const Heroes: NextPage<Props> = ({ heroes, roles }) => {
             </h2>
             <div className="flex flex-wrap -m-4">
               {heroes
+                .filter((hero) =>
+                  role === 'All' ? true : hero.roles.includes(role)
+                )
                 .filter((hero) => hero.attributes.primary === attribute)
                 .map((hero, index) => (
                   <Link href={`/heroes/${hero.slug}`} key={index}>
                     <a
-                      className={`m-4 w-hero-sm md:w-hero-md ${
-                        role === 'all'
-                          ? ''
-                          : hero.roles.join().toLowerCase().includes(role)
-                          ? ''
-                          : 'opacity-25'
-                      }`}>
+                      className="m-4 w-hero-sm md:w-hero-md group"
+                      style={{
+                        zIndex: 100 - index
+                      }}>
                       <img
-                        className="bg-gray-900 rounded w-full"
+                        className="bg-gray-900 rounded w-full z-0"
                         src={hero.image}
                       />
+                      <div
+                        className="hidden md:block -ml-4 -mb-4 text-white absolute z-10 bg-gray-900 text-sm px-3 py-2 rounded opacity-0 transition-opacity duration-300 pointer-events-none group-hover:opacity-100"
+                        style={{
+                          bottom: '100%',
+                          left: '100%',
+                          width: 'max-content'
+                        }}>
+                        <span>{hero.name}</span>
+                      </div>
                     </a>
                   </Link>
                 ))}
