ddb6a228298bb933a31256aa5748dbe1b6e755d3
Ali Zahid <ali.zahid@live.com>
2020-05-13 20:11:13 +0400

    Design updated

--- components/header.tsx
+++ components/header.tsx
@@ -16,28 +16,33 @@ export const Header: FunctionComponent = () => {
       </Link>
       <nav className="mt-6 md:mt-0">
         <Link href="/">
-          <a className={`${path === '/' ? 'text-white' : ''}`}>Search</a>
+          <a
+            className={`p-3 rounded ${
+              path === '/' ? 'text-white bg-red-500' : ''
+            }`}>
+            Search
+          </a>
         </Link>
         <Link href="/heroes">
           <a
-            className={`ml-4 ${
-              path.indexOf('/heroes') === 0 ? 'text-white' : ''
+            className={`ml-4 p-3 rounded ${
+              path.indexOf('/heroes') === 0 ? 'text-white bg-red-500' : ''
             }`}>
             Heroes
           </a>
         </Link>
         <Link href="/items">
           <a
-            className={`ml-4 ${
-              path.indexOf('/items') === 0 ? 'text-white' : ''
+            className={`ml-4 p-3 rounded ${
+              path.indexOf('/items') === 0 ? 'text-white bg-red-500' : ''
             }`}>
             Items
           </a>
         </Link>
         <Link href="/abilities">
           <a
-            className={`ml-4 ${
-              path.indexOf('/abilities') === 0 ? 'text-white' : ''
+            className={`ml-4 p-3 rounded ${
+              path.indexOf('/abilities') === 0 ? 'text-white bg-red-500' : ''
             }`}>
             Abilities
           </a>
